7d40fe70c8ad0a24cf36ff2db33c2cb0a5a600b1
Tomasz Wojdat <tpwojdat@gmail.com>
2025-12-29 03:21:25 +0100

    Implement filtering by categories
    Also, improve logging.

--- app/main.go
+++ app/main.go
@@ -7,9 +7,65 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+const (
+	// Movies
+	MOVIES             = "movies"
+	MOVIES_BD_FULL     = "movies_bd_full"
+	MOVIES_BD_REMUX    = "movies_bd_remux"
+	MOVIES_X264        = "movies_x264"
+	MOVIES_X264_3D     = "movies_x264_3d"
+	MOVIES_X264_4K     = "movies_x264_4k"
+	MOVIES_X264_720    = "movies_x264_720"
+	MOVIES_X265        = "movies_x265"
+	MOVIES_X265_4K     = "movies_x265_4k"
+	MOVIES_X265_4K_HDR = "movies_x265_4k_hdr"
+	MOVIES_XVID        = "movies_xvid"
+	MOVIES_XVID_720    = "movies_xvid_720"
+
+	// TV
+	TV     = "tv"
+	TV_SD  = "tv_sd"
+	TV_UHD = "tv_uhd"
+
+	// Games
+	GAMES_PC_ISO  = "games_pc_iso"
+	GAMES_PC_RIP  = "games_pc_rip"
+	GAMES_PS3     = "games_ps3"
+	GAMES_PS4     = "games_ps4"
+	GAMES_XBOX360 = "games_xbox360"
+
+	// Music
+	MUSIC_FLAC = "music_flac"
+	MUSIC_MP3  = "music_mp3"
+
+	// Books
+	EBOOKS = "ebooks"
+
+	// Software
+	SOFTWARE_PC_ISO = "software_pc_iso"
+
+	// Adult
+	XXX = "xxx"
+)
+
+var CATEGORY_MAP = map[string][]string{
+	"Movies": {
+		MOVIES, MOVIES_BD_FULL, MOVIES_BD_REMUX, MOVIES_X264, MOVIES_X264_3D,
+		MOVIES_X264_4K, MOVIES_X264_720, MOVIES_X265, MOVIES_X265_4K,
+		MOVIES_X265_4K_HDR, MOVIES_XVID, MOVIES_XVID_720,
+	},
+	"TV":       {TV, TV_SD, TV_UHD},
+	"Games":    {GAMES_PC_ISO, GAMES_PC_RIP, GAMES_PS3, GAMES_PS4, GAMES_XBOX360},
+	"Music":    {MUSIC_FLAC, MUSIC_MP3},
+	"Books":    {EBOOKS},
+	"Software": {SOFTWARE_PC_ISO},
+	"Adult":    {XXX},
+}
 
 type Result struct {
 	Title  string `json:"title"`
@@ -62,6 +118,22 @@ func getResults(db *sql.DB) http.HandlerFunc {
 
 		category := query.Get("category")
 
+		cats, ok := CATEGORY_MAP[category]
+		if !ok {
+			cats = nil
+		}
+
+		var catFilter string
+		if cats != nil {
+			for i := range cats {
+				cats[i] = fmt.Sprintf(`"%s"`, cats[i])
+			}
+			catFilter = fmt.Sprintf(" AND i.cat IN (%s)", strings.Join(cats, ","))
+		} else {
+			catFilter = ""
+		}
+		fmt.Println(catFilter)
+
 		sortCol := query.Get("sort_col")
 		if sortCol == "" {
 			sortCol = "title"
@@ -72,30 +144,43 @@ func getResults(db *sql.DB) http.HandlerFunc {
 			sortDir = "asc"
 		}
 
-		var count int
-		err = db.QueryRow(`
+		log.Printf(`
+			Query parameters:
+				search_query=%s
+				page=%d
+				per_page=%d
+				cat=%s
+				sort_col=%s
+				sort_dir=%s`,
+			searchQuery, page, perPage, category, sortCol, sortDir)
+
+		queryStrCount := fmt.Sprintf(`
 			SELECT COUNT(*) FROM items_fts
-			WHERE items_fts MATCH (?)`,
-			searchQuery,
-		).Scan(&count)
+			JOIN items i ON i.rowid = items_fts.rowid
+			WHERE items_fts MATCH "%s"%s`,
+			searchQuery, catFilter,
+		)
+		log.Printf("COUNT(*) query: %s", queryStrCount)
+		var count int
+		err = db.QueryRow(queryStrCount).Scan(&count)
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Print(count)
+		log.Printf("Total count: %d", count)
 
 		offset := (page - 1) * perPage
 
-		// TODO: cat_filter needs to be implemented
 		queryStr := fmt.Sprintf(`
 			SELECT i.title, i.cat, i.dt, i.size, i.hash
 			FROM items_fts
 			JOIN items i ON i.rowid = items_fts.rowid
-			WHERE items_fts MATCH "%s"
+			WHERE items_fts MATCH "%s"%s
 			ORDER BY i."%s" %s
 			LIMIT %d OFFSET %d
 			`,
-			searchQuery, sortCol, sortDir, perPage, offset,
+			searchQuery, catFilter, sortCol, sortDir, perPage, offset,
 		)
+		log.Printf("SELECT query: %s", queryStr)
 
 		rows, err := db.Query(queryStr)
 		if err != nil {
@@ -109,14 +194,23 @@ func getResults(db *sql.DB) http.HandlerFunc {
 			var title string
 			var cat string
 			var dt string
-			var size int
+			var size sql.NullInt64
 			var hash string
 			err := rows.Scan(&title, &cat, &dt, &size, &hash)
 			if err != nil {
 				log.Fatal(err)
 			}
+
+			var intSize int
+			if size.Valid {
+				intSize = int(size.Int64)
+			} else {
+				intSize = 0 // or whatever default you want
+			}
+
 			magnet := fmt.Sprintf("magnet:?xt=urn:btih:%s&dn=%s", hash, title)
-			res := Result{title, cat, dt, size, magnet}
+
+			res := Result{title, cat, dt, intSize, magnet}
 			results = append(results, res)
 		}
 
@@ -128,7 +222,5 @@ func getResults(db *sql.DB) http.HandlerFunc {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(response)
 
-		log.Printf("Query parameters:\nsearch_query=%s\npage=%d\nper_page=%d\ncat=%s\nsort_col=%s\nsort_dir=%s\n",
-			searchQuery, page, perPage, category, sortCol, sortDir)
 	}
 }
